435828b5850e212878077bf4e29008c2d7a5b705
nicolas-hermosilla <51442132+nicolas-hermosilla@users.noreply.github.com>
2021-02-06 23:05:43 +0100

    Update findSection.js

--- components/findSection.js
+++ components/findSection.js
@@ -87,7 +87,7 @@ const FindSection = (props) => {
                         <path d="M15.787 7.531c-5.107 2.785-12.72 9.177-15.787 15.469h2.939c.819-2.021 2.522-4.536 3.851-5.902 8.386 3.747 17.21-2.775 17.21-11.343 0-1.535-.302-3.136-.92-4.755-2.347 3.119-5.647 1.052-10.851 1.625-7.657.844-11.162 6.797-8.764 11.54 3.506-3.415 9.523-6.38 12.322-6.634z" /></svg>
                     <motion.h1 initial={{ opacity: 0, x: -50 }} animate={{ opacity: 1, x: 0 }}
                         transition={{ duration: 1 }}
-                        className="text-3xl font-bold tracking-wide md:text-5xl">Commander un trajet</motion.h1>
+                        className="text-3xl font-bold tracking-wide md:text-5xl">Commander votre trajet</motion.h1>
                     <p className="mt-2">Déplacez vous sans stress avec Aerocap</p>
                 </div>
                 <div className="justify-end hidden col-span-1 md:flex">
