073a985ad7b6f316f83f3eda8d61abecefe1188f
VincentG <vincent.gougeon@ynov.com>
2021-02-07 23:52:43 +0100

    fix

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css"></link>
       </Head>
-      <Header />
+      {/* <Header /> */}
       <Scroller />
       <HomeSection />
       <FindSection />
