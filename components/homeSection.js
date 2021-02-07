31f637ac16fc8657e13f12416fd7572fba802233
nicolas-hermosilla <51442132+nicolas-hermosilla@users.noreply.github.com>
2021-02-07 19:43:02 +0100

    Update homeSection.js

--- components/homeSection.js
+++ components/homeSection.js
@@ -20,7 +20,7 @@ const HomeSection = (props) => {
                         animate={{ opacity: 1, x: 0, height: "auto"}} exit={{ opacity: 0, x: 50, height:0}}
                         transition={{ type:'tween', duration: 0.2}}>
                             <p className="mt-2 text-width">AeroCap est une capsule hybride innovante qui vous apportera une expérience hors du commun !</p>
-                            <p className="mt-2 text-width">En se déplaçant de manière aérienne grâce à l’énergie solaire et à l’hydrogène, AeroCap vous permettra de voyager en toute convivialité où vous le voulez dans toute la France métropolitaine.</p>
+                            <p className="mt-2 text-width">En se déplaçant de manière aérienne grâce à l’hydrogène et à l’énergie solaire, AeroCap vous permettra de voyager en toute convivialité où vous le voulez dans toute la France métropolitaine.</p>
                             <div className="flex flex-wrap">
                                 <a href="#find" className="flex items-center justify-center h-10 px-3 mt-5 mr-2 bg-green-500 border-2 border-blue-300 rounded-md hover:bg-blue-600 whitespace-nowrap w-min">
                                     Choisir un trajet
